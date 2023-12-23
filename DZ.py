29d8bbec8468efc26e30b5b1d1b6e8dbe0617eba
Crxckovich <artpeshov281402@gmail.com>
2023-12-23 19:42:15 +0300

    Create DZ.py

---
+++ DZ.py
@@ -0,0 +1,50 @@
+# 1 ЗАДАНИЕ
+# num1 = float(input('Введите первое число: '))
+# num2 = float(input('Введите второе число: '))
+# num3 = float(input('Введите третье число: '))
+
+# choice = input('Выберите операцию (сумма/произведение): ')
+
+# if choice == 'сумма':
+#     result = num1 + num2 + num3
+#     print(f'Сумма трех чисел: {result}')
+# elif choice == 'произведение':
+#     result = num1 * num2 * num3
+#     print(f'Произведение трех чисел: {result}')
+# else:
+#     print('Неверный выбор операции. Пожалуйста, выберите 'сумма' или 'произведение'.')
+
+# 2 ЗАДАНИЕ
+# num1 = float(input('Введите первое число: '))
+# num2 = float(input('Введите второе число: '))
+# num3 = float(input('Введите третье число: '))
+
+# choice = input('Выберите операцию (максимум/минимум/среднее): ')
+
+# if choice == 'максимум':
+#     result = max(num1, num2, num3)
+#     print(f'Максимум из трех чисел: {result}')
+# elif choice == 'минимум':
+#     result = min(num1, num2, num3)
+#     print(f'Минимум из трех чисел: {result}')
+# elif choice == 'среднее':
+#     result = (num1 + num2 + num3) / 3
+#     print(f'Среднее из трех чисел: {result}')
+# else:
+#     print('Неверный выбор операции. Пожалуйста, выберите 'максимум', 'минимум' или 'среднее'.')
+
+# 3 ЗАДАНИЕ
+# meters = float(input('Введите количество метров: '))
+# choice = input('Выберите единицу для перевода (мили/дюймы/ярды): ')
+
+# if choice == 'мили':
+#     miles = meters / 1609.34
+#     print(f'{meters} метров = {miles} миль')
+# elif choice == 'дюймы':
+#     inches = meters * 39.37
+#     print(f'{meters:0.2f} метров = {inches:0.2f} дюймов')
+# elif choice == 'ярды':
+#     yards = meters * 1.094
+#     print(f'{meters} метров = {yards:0.2f} ярдов')
+# else:
+#     print('Неверный выбор единицы для перевода. Пожалуйста, выберите 'мили', 'дюймы' или 'ярды'.')
